e9a112902ab55b9e300140615518e7f213761f7d
W <rw.2021@yandex.ru>
2024-07-30 17:56:34 +0300

    Домашняя работа по уроку "Атрибуты и методы объекта"

---
+++ module_4_2.py
@@ -0,0 +1,32 @@
+# Домашняя работа по уроку "Атрибуты и методы объекта"
+
+# Цель: применить на практике знания о классах, объектах и их атрибутах.
+
+# Задача "Developer - не только разработчик"
+
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if new_floor < 1 or new_floor > self.number_of_floors:
+            print("Такого этажа не существует")
+            return
+        for i in range(new_floor):
+            print(i + 1)
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
+
+# >>> 1
+# >>> 2
+# >>> 3
+# >>> 4
+# >>> 5
+# >>> Такого этажа не существует
+
+print ('\nРабота завершена.')
